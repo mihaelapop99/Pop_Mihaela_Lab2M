4d444fc6ae935b957c16e62a3f46e4101b80260b
Mihaela <Mihaela@DESKTOP-JK9UMAB>
2021-10-05 21:26:41 +0300

    Add project files.

---
+++ Pop_Mihaela_Lab2M/Models/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pop_Mihaela_Lab2M.Models
+{
+    public class Book
+    {
+  
+            public int ID { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public decimal Price { get; set; }
+            public ICollection<Order> Orders { get; set; }
+
+      
+    }
+}
---
+++ Pop_Mihaela_Lab2M/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Pop_Mihaela_Lab2M.Models
+{
+    public class Order
+    {
+        
+       
+            public int OrderID { get; set; }
+            public int CustomerID { get; set; }
+            public int BookID { get; set; }
+
+            public Customer Customer { get; set; }
+            public Book Book { get; set; }
+       
+    }
+}
