0bebb27875975ca3fdd7dadb0ed1ddaceaf7271e
Mihaela Pop <mihaelapop919@gmail.com>
2021-10-17 11:31:06 +0300

    Pop_Mihaela_Lab3M

--- Pop_Mihaela_Lab2M/Models/Order.cs
+++ Pop_Mihaela_Lab2M/Models/Order.cs
@@ -12,7 +12,7 @@ namespace Pop_Mihaela_Lab2M.Models
             public int OrderID { get; set; }
             public int CustomerID { get; set; }
             public int BookID { get; set; }
-
+            public DateTime OrderDate { get; set; }
             public Customer Customer { get; set; }
             public Book Book { get; set; }
        
